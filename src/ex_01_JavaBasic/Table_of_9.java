0a92889434e81e86892d5639e7e6af2c1ec7c719
Alok Tiwari <alok21@gmail.com>
2025-03-11 18:11:27 +0530

    Task 11th March

---
+++ src/ex_01_JavaBasic/Table_of_9.java
@@ -0,0 +1,27 @@
+package ex_01_JavaBasic;
+
+public class Table_of_9 {
+    public static void main(String[] args) {
+        System.out.println("Table of 9-------------------");
+        System.out.printf("9*1=9");
+        System.out.println("");
+        System.out.printf("9*2=18");
+        System.out.println("");
+        System.out.printf("9*3=27");
+        System.out.println("");
+        System.out.printf("9*4=36");
+        System.out.println("");
+        System.out.printf("9*5=45");
+        System.out.println();
+        System.out.printf("9*6=54");
+        System.out.println();
+        System.out.printf("9*7=63");
+        System.out.println();
+        System.out.printf("9*8=72");
+        System.out.println();
+        System.out.printf("9*9=81");
+        System.out.println();
+        System.out.printf("9*10=90");
+
+    }
+}
