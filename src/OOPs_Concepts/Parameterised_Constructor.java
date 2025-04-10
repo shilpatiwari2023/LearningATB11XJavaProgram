4eca48427002b8353d8bcb3d29abab7c841a1ba4
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-04-10 13:27:00 +0530

    10 ap

--- src/OOPs_Concepts/Parameterised_Constructor.java
+++ src/OOPs_Concepts/Parameterised_Constructor.java
@@ -6,6 +6,21 @@ public class Parameterised_Constructor {
         System.out.println(c1.name);
         System.out.println(c1.model);
         System.out.println(c1.year);
+        System.out.println("-----------------------------");
+        Car1 c2 = new Car1("tesla","xxx",2010);
+        System.out.println(c2.name);
+        System.out.println(c2.model);
+        System.out.println(c2.year);
+
+        System.out.println("----------------------------------");
+        Car1 c3 = new Car1("wagonr", "madhvan", 2013);
+        System.out.println(c3.name);
+        System.out.println(c3.model);
+        System.out.println(c3.year);
+
+
+
+
 
 
 
@@ -24,8 +39,8 @@ class Car1{
         model = "Ascent";
     }
     Car1(String name,String model, int year){
-        this.name = "tesla";
-        this.model = "XXX";
-        this.year = 2000;
+        this.name = name;
+        this.model = model;
+        this.year = year;
     }
 }
