d3952e58e8d11215fb7ce29430ce74c625a8517f
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-04-15 07:31:38 +0530

    15

---
+++ src/Inheritance/single_inheritance.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+public class single_inheritance {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Father{
+    int gold_f = 1000;
+
+    void bhk2(){
+        System.out.println("Father- 2 bhk");
+}
+}
+
+class Son{
+     sout
