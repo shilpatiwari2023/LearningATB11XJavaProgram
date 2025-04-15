66ee03bb791dc5a78caf3d503ba4443a2792c0eb
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-04-15 08:07:46 +0530

    .1

--- src/Inheritance/single_inheritance.java
+++ src/Inheritance/single_inheritance.java
@@ -2,11 +2,17 @@ package Inheritance;
 
 public class single_inheritance {
     public static void main(String[] args) {
+      Father f1 = new Father();
+        System.out.println(f1.gold_f);
+        f1.bhk2();
 
+        Son s1 = new Son();
+        s1.bhk3();
+        System.out.println(s1.gold_f);
+        s1.bhk2();
     }
-}
 
-class Father{
+static class Father{
     int gold_f = 1000;
 
     void bhk2(){
@@ -14,5 +20,9 @@ class Father{
 }
 }
 
-class Son{
-     sout
+static class Son extends Father{
+        void bhk3(){
+            System.out.println("Son-3BHK");
+        }
+}
+}
