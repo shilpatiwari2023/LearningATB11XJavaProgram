234fce78408226460a1ff2b619ff594db39a22a4
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-05-06 15:16:21 +0530

    6.1

--- src/Strings/Pyramid_pattern_star.java
+++ src/Strings/Pyramid_pattern_star.java
@@ -4,6 +4,15 @@ public class Pyramid_pattern_star {
     public static void main(String[] args) {
 
         int rows = 5;
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= rows - 1; j++){
+                System.out.println(" ");
+            }
+            for(int k = 1; k <= (2 * i -1); k++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
 
     }
 }
