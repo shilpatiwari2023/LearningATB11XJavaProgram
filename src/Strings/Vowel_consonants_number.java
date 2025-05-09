10de5a95a7f37c0605c3c99d01cccb2b681321a6
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-05-09 15:12:19 +0530

    9.1

--- src/Strings/Vowel_consonants_number.java
+++ src/Strings/Vowel_consonants_number.java
@@ -4,6 +4,21 @@ public class Vowel_consonants_number {
     public static void main(String[] args) {
          int vowel_count=0;
          int consonants_count=0;
+         int i;
+         String str = "My name is Shilpa Tiwari";
+
+         System.out.println(str.toLowerCase());
+
+         for(i = 0; i< str.length(); i++){
+             char ch = str.charAt(i);
+             if(ch == 'a' ||  ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                 vowel_count ++ ;
+             } else {
+                 consonants_count ++;
+             }
+         }
+        System.out.println("No of vowels = " + vowel_count);
+        System.out.println("No of consonants " + consonants_count);
 
 
     }
