d85dd7f0ade5373bfe30c1deea552d55ffa351fb
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-05-09 14:48:38 +0530

    9

---
+++ src/Strings/Vowel_consonants_number.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Vowel_consonants_number {
+    public static void main(String[] args) {
+         int vowel_count=0;
+         int consonants_count=0;
+
+
+    }
+}
