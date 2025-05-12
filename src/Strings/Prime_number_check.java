dea2e1ac67c9e81fc6a60f4c0f3cdd55ce302f8d
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-05-12 14:46:09 +0530

    12

---
+++ src/Strings/Prime_number_check.java
@@ -0,0 +1,14 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Prime_number_check {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number to check = ");
+        int number = scanner.nextInt();
+
+        if(number % number == 0 )
+    }
+}
